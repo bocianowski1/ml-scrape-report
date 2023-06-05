b08bca26009e9ca909768858547335e7ead121fe
torgerbocianowski <torgerboc@gmail.com>
2023-06-05 18:40:55 +0200

    add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,11 @@
 FROM python:3.9.13-slim
 
-WORKDIR /the/workdir/path
+WORKDIR /code
+
+COPY requirements.txt ./
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+CMD [ "python", "main.py" ]
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import asyncio
 import time
 
 # from report.pdf import PDF
-from data.scraping.scrape import scrape
-from data.database.db import drop_all_tables, drop_table
-from data.ml.sentiment_analysis import load_model
+# from data.scraping.scrape import scrape
+# from data.database.db import drop_all_tables, drop_table
+# from data.ml.sentiment_analysis import load_model
 
 
 # def make_report():
@@ -15,13 +15,13 @@ from data.ml.sentiment_analysis import load_model
 
 def main(test=False) -> None:
     start_time = time.time()
-    drop_all_tables(test)
+    # drop_all_tables(test)
     load_dotenv()
     print("Starting...")
-    model_params = load_model()
+    # model_params = load_model()
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(scrape(test, model_params))
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(scrape(test, model_params))
 
     elapsed_time = time.time() - start_time
     print(f"\nElapsed time: {elapsed_time}")
